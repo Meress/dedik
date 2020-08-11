c53c47672447615bfa59da9d05babe709bda57bf
Artyom <temik54@rambler.ru>
2020-08-11 16:54:54 +0300

    2

--- site/www/index.php
+++ site/www/index.php
@@ -113,12 +113,3 @@
 	</body>
 </html>
 
-<?php
-$ip = $_SERVER['REMOTE_ADDR']; //получаем IP адрес клиента
-$client = $_SERVER['HTTP_USER_AGENT']; //получаем идентификатор HTTP клиента
-$today = date("Y.m.d H:i:s"); //получаем текущие дату и время
-$f = fopen("log.csv","a"); //открываем файл для добавления данных
-$param = $_REQUEST['a']; //получаем значение посланной переменной "а"
-fwrite($f,"$today; $ip; $client; sensor=$param\r\n-----------------\r\n"); //запись данных в файл
-fclose($f); //закрываем файл
-?>
